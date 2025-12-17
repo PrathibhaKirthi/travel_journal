b8ce58c6a5881f66eb66655e17539bf5bc98b233
PrathibhaKirthi <prathibhakirthi@gmail.com>
2025-12-17 16:13:00 +0000

    Disable Action Cable / Solid Cable for production

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module TravelJournal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
+    config.action_cable.mount_path = nil
+
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
